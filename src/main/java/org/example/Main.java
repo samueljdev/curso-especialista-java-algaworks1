77b183b9cb0083f35e16cae3430eaf86bc22ff38
samue <samuel.jdev11@gmail.com>
2024-03-11 19:30:53 -0400

    Acessando atributos de objetos

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,28 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Carro meuCarro = new Carro();
+        meuCarro.anoFabricacao = 2021;
+        meuCarro.cor = "Preto";
+        meuCarro.fabricante = "Honda";
+        meuCarro.modelo = "HR-V";
+
+        Carro seuCarro = new Carro();
+        seuCarro.anoFabricacao = 2022;
+        seuCarro.cor = "Branco";
+        seuCarro.fabricante = "BMW";
+        seuCarro.modelo = "X6";
+
+        System.out.println("Meu carro");
+        System.out.println("---------");
+        System.out.printf("Modelo: %s%n", meuCarro.modelo);
+        System.out.printf("Ano: %d%n", meuCarro.anoFabricacao);
+
+        System.out.println();
+
+        System.out.println("Seu carro");
+        System.out.println("---------");
+        System.out.printf("Modelo: %s%n", seuCarro.modelo);
+        System.out.printf("Ano: %d%n", seuCarro.anoFabricacao);
     }
 }
