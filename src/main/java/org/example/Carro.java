ddacaf8f08fc0652c95390f5b9f8636b93eaa608
samue <samuel.jdev11@gmail.com>
2024-03-15 17:36:58 -0400

    Inicialização de objetos em variáveis de instância

---
+++ src/main/java/org/example/Carro.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Carro {
+    String fabricante = "Ford";
+    String modelo;
+    String cor;
+    int anoFabricacao = 2022;
+    Pessoa proprietario = new Pessoa();
+}
