2f3b3955162b5079012305441707c73982eb5dd2
samue <samuel.jdev11@gmail.com>
2024-03-17 20:43:13 -0400

    Criando e invocando um método

---
+++ src/main/java/org/example/Carro.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Carro {
+    String fabricante;
+    String modelo;
+    String cor;
+    int anoFabricacao;
+    Pessoa proprietario;
+
+    void calcularValorRevenda() {
+        System.out.printf("Calcular valor de revenda de: %s %d%n", modelo, anoFabricacao);
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,31 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Pessoa primeiraPessoa = new Pessoa();
+        primeiraPessoa.nome = "Samuel Lisboa";
+        primeiraPessoa.cpf = "111.222.333-44";
+        primeiraPessoa.anoNascimento = 1981;
+
+        Pessoa segundaPessoa = new Pessoa();
+        segundaPessoa.nome = "João das Exemplo";
+        segundaPessoa.cpf = "999.999.999-99";
+        segundaPessoa.anoNascimento = 1992;
+
+        Carro meuCarro = new Carro();
+        meuCarro.anoFabricacao = 2021;
+        meuCarro.cor = "Preto";
+        meuCarro.fabricante = "Honda";
+        meuCarro.modelo = "HR-V";
+        meuCarro.proprietario = primeiraPessoa;
+
+        Carro seuCarro = new Carro();
+        seuCarro.anoFabricacao = 2022;
+        seuCarro.cor = "Branco";
+        seuCarro.fabricante = "BMW";
+        seuCarro.modelo = "X6";
+        seuCarro.proprietario = segundaPessoa;
+
+        meuCarro.calcularValorRevenda();
+        seuCarro.calcularValorRevenda();
     }
 }
