02014ed66b66800552e6b770e23e5bd1e6bc6e40
samue <samuel.jdev11@gmail.com>
2024-03-23 00:31:21 -0400

    Métodos que retornam objetos

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,16 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Paciente paciente = new Paciente();
+        paciente.altura = 1.82;
+        paciente.peso = 175;
+
+        IndiceMassaCorporal imc = paciente.calcularIndiceMassaCorporal();
+
+        if (imc.resultado >= 30) {
+            System.out.printf("Paciente com altura de %.2f e peso de %.2f está com obesidade%n", imc.altura, imc.peso);
+        }
+
+        System.out.printf("IMC: %.2f%n", imc.resultado);
     }
 }
