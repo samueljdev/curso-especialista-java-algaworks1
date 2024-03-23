45fc2614778f83ab491dd6df3fb1b70b50836eb0
samue <samuel.jdev11@gmail.com>
2024-03-23 18:15:22 -0400

    Métodos com argumentos

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,18 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        CalculadoraImc calculadoraImc = new CalculadoraImc();
+        double peso = 75;
+        double altura = 1.82;
+
+        IndiceMassaCorporal imc = calculadoraImc.calcular(peso, altura);
+
+        if (imc.estaComObesidade()) {
+            System.out.printf("Paciente com altura de %.2f e peso de %.2f está com obesidade%n", imc.altura, imc.peso);
+        } else if (imc.estaAbaixoDoPesoIdeal()) {
+            System.out.printf("Paciente com altura de %.2f e peso de %.2f está abaixo do peso ideal %n", imc.altura, imc.peso);
+        }
+
+        System.out.printf("IMC: %.2f%n", imc.resultado);
     }
 }
