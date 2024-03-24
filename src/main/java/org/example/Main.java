b1d12e544ba8b66d2052b90862fcbef703b5cba5
samue <samuel.jdev11@gmail.com>
2024-03-23 22:35:38 -0400

    Métodos que alteram variável de instância

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,23 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Aeronave aviaoGol = new Aeronave();
+        aviaoGol.totalAssentos = 100;
+
+        aviaoGol.desativar();
+        aviaoGol.ativar();
+
+        aviaoGol.reservarAssentos(50);
+
+        System.out.printf("GOL (%s): %d assentos disponíveis%n",
+                aviaoGol.ativo ? "Ativo" : "Inativo", aviaoGol.calcularAssentosDisponiveis());
+
+        Aeronave aviaoLatam = new Aeronave();
+        aviaoLatam.totalAssentos = 120;
+
+        aviaoLatam.reservarAssentos(90);
+
+        System.out.printf("LATAM (%s): %d assentos disponíveis%n",
+                aviaoLatam.ativo ? "Ativo" : "Inativo", aviaoLatam.calcularAssentosDisponiveis());
     }
 }
