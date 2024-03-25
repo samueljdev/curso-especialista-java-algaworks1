b89d7426d0a3bec3a542226ce5e63063aee63b97
samue <samuel.jdev11@gmail.com>
2024-03-25 19:33:39 -0400

    composição de objetos e métodos

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,19 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Funcionario funcionario = new Funcionario();
+        funcionario.nome = "João da Silva";
+        funcionario.quantidadeFilhos = 2;
+
+        ContratoTrabalho contratoTrabalho = new ContratoTrabalho();
+        contratoTrabalho.funcionario = funcionario;
+        contratoTrabalho.valorHoraNormal = 51.8;
+        contratoTrabalho.valorHoraExtra = 60.5;
+
+        FolhaPagamento folhaPagamento = new FolhaPagamento();
+
+        double salarioDevido = folhaPagamento.calcularSalario(160, 10, contratoTrabalho);
+
+        System.out.printf("Salário devido: R$ %.2f%n", salarioDevido);
     }
 }
