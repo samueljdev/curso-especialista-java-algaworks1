f1bd8ccf9680bcd3ee5be314dc75fd8aa746d385
samue <samuel.jdev11@gmail.com>
2024-03-25 16:13:37 -0400

    Sobrecarga de métodos

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,13 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Visitante novoVisitante = new Visitante();
+        novoVisitante.nome = "João";
+        novoVisitante.idade = 15;
+
+        CadastroPortaria cadastroPortaria = new CadastroPortaria();
+        // sobrecarga de metodos, usando dois metodos cadastrar
+        cadastroPortaria.cadastrar(novoVisitante);
+        cadastroPortaria.cadastrar(novoVisitante, 2);
     }
 }
