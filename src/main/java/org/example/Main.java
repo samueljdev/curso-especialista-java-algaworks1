bb0ee01070551c95552481b8d627bf745d08427c
samue <samuel.jdev11@gmail.com>
2024-03-25 16:16:58 -0400

    Inferência de tipo de variável local/

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,12 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        var novoVisitante = new Visitante(); // var como inferencia
+
+        novoVisitante.nome = "João";
+        novoVisitante.idade = 15;
+
+        var cadastroPortaria = new CadastroPortaria(); // var como inferencia
+        int codigoVisitante = cadastroPortaria.cadastrar(novoVisitante, 2);
     }
 }
