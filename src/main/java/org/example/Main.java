4763f8225cf52251b32ccaeb0097b94ae2dfecc4
samue <samuel.jdev11@gmail.com>
2024-03-25 19:25:15 -0400

    Método de instância alterando variável estática

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,13 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Produto produto1 = new Produto();
+        Produto produto2 = new Produto();
+
+        produto1.alterarCustoEmbalagem(12);
+        produto2.alterarCustoEmbalagem(20);
+
+        produto1.imprimirCustoEmbalagem();
+        produto2.imprimirCustoEmbalagem();
     }
 }
